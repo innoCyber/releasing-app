2d1550646f3aee6c6bda076007470c30c9de3c06
KryptKode <paulakhamiogu@gmail.com>
2019-02-27 10:55:55 +0100

    Added base util classes

---
+++ app/src/main/java/it/ounet/releasing/utils/ReleasingGlideAppModule.kt
@@ -0,0 +1,7 @@
+package it.ounet.releasing.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class ReleasingGlideAppModule : AppGlideModule()
---
+++ app/src/main/java/it/ounet/releasing/utils/SpannableHelper.kt
@@ -0,0 +1,22 @@
+package it.ounet.releasing.utils
+
+
+import android.content.Context
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.URLSpan
+import androidx.core.content.ContextCompat
+import it.ounet.releasing.R
+
+class SpannableHelper {
+
+    fun formatToLink(url: String?, context: Context?): SpannableString {
+        val spannableUrl = SpannableString(url)
+        spannableUrl.setSpan(URLSpan(url), 0, url?.length ?: 0, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableUrl.setSpan(ForegroundColorSpan(ContextCompat.getColor(context!!, R.color.colorAccent)), 0, url?.length ?: 0, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return spannableUrl
+    }
+
+
+}
