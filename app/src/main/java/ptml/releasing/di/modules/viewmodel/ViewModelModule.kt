5a2b772bf4eda7e777ca069e1969db466c270019
KryptKode <paulakhamiogu@gmail.com>
2019-03-12 16:08:54 +0100

    Added view model modules Added base response class

---
+++ app/src/main/java/ptml/releasing/di/modules/viewmodel/ViewModelModule.kt
@@ -0,0 +1,24 @@
+package ptml.releasing.di.modules.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import ptml.releasing.di.mapkeys.ViewModelKey
+import ptml.releasing.ui.login.LoginViewModel
+import ptml.releasing.ui.setup.SetupActivityViewModel
+
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SetupActivityViewModel::class)
+    abstract fun bindSetupViewModel(myViewModel: SetupActivityViewModel): ViewModel
+
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(LoginViewModel::class)
+    abstract fun bindLoginViewModel(myViewModel: LoginViewModel): ViewModel
+}
