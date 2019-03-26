7b70bf2774e8d756a21fef9661e53d692512dc4f
KryptKode <paulakhamiogu@gmail.com>
2019-03-26 07:48:10 +0100

    Home layout completed

--- app/src/main/java/ptml/releasing/home/view/HomeActivity.kt
+++ app/src/main/java/ptml/releasing/home/view/HomeActivity.kt
@@ -1,19 +1,46 @@
 package ptml.releasing.home.view
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import ptml.releasing.BR
 import ptml.releasing.R
-import ptml.releasing.app.base.BaseFragmentActivity
+import ptml.releasing.app.base.BaseActivity
 import ptml.releasing.databinding.ActivityHomeBinding
+import ptml.releasing.home.viewmodel.HomeViewModel
 
-class HomeActivity : BaseFragmentActivity< ActivityHomeBinding>() {
+
+class HomeActivity : BaseActivity<HomeViewModel, ActivityHomeBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(binding.appBarHome.toolbar)
+        val toggle = ActionBarDrawerToggle(
+                this, binding.drawerLayout, binding.appBarHome.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(navigationListener)
 
 
     }
+
+    private val navigationListener = object : NavigationView.OnNavigationItemSelectedListener {
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            when (item.itemId) {
+                R.id.nav_preferences -> {
+                    //TODO handle nav
+                }
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            return true
+        }
+    }
+
     override fun getLayoutResourceId() = R.layout.activity_home
 
     override fun getBindingVariable() = BR._all
+
+    override fun getViewModelClass() = HomeViewModel::class.java
 }
