dfc492766dfec6b8682b89979991d3592579d76d
KryptKode <paulakhamiogu@gmail.com>
2019-10-08 19:15:45 +0100

    Implemented logic to handle events so they are not triggered after they have been handled

--- app/src/main/java/ptml/releasing/admin_config/viewmodel/AdminConfigViewModel.kt
+++ app/src/main/java/ptml/releasing/admin_config/viewmodel/AdminConfigViewModel.kt
@@ -8,47 +8,47 @@ import kotlinx.coroutines.withContext
 import ptml.releasing.app.base.BaseViewModel
 import ptml.releasing.app.data.Repository
 import ptml.releasing.app.utils.AppCoroutineDispatchers
+import ptml.releasing.app.utils.Event
 import ptml.releasing.app.utils.remoteconfig.RemoteConfigUpdateChecker
 import javax.inject.Inject
 
 class AdminConfigViewModel @Inject constructor(repository: Repository, appCoroutineDispatchers: AppCoroutineDispatchers,
                                                updateChecker: RemoteConfigUpdateChecker
-) : BaseViewModel(updateChecker, repository, appCoroutineDispatchers) {
+) :
+    BaseViewModel(updateChecker, repository, appCoroutineDispatchers) {
 
-    private val _openConfig = MutableLiveData<Unit>()
-    private val _openPrinterSettings = MutableLiveData<Unit>()
-    private val _openDownloadDamages = MutableLiveData<Boolean>()
-    private val _openQuickRemark = MutableLiveData<Boolean>()
-    private val _openErrorLogs = MutableLiveData<Unit>()
-    private val _serverUrl = MutableLiveData<String?>()
+    private val _openConfig = MutableLiveData<Event<Unit>>()
+    private val _openPrinterSettings = MutableLiveData<Event<Unit>>()
+    private val _openDownloadDamages = MutableLiveData<Event<Boolean>>()
+    private val _openQuickRemark = MutableLiveData<Event<Boolean>>()
+    private val _serverUrl = MutableLiveData<Event<String?>>()
+    private val _openSearch = MutableLiveData<Event<Boolean>>()
 
     private val _firstTimeLogin = MutableLiveData<Boolean>()
     private val _firstTimeFindCargo = MutableLiveData<Boolean>()
     private var first = true
-    private val _openSearch = MutableLiveData<Boolean>()
 
-    val openConfig: LiveData<Unit> = _openConfig
-    val openQuickRemark: LiveData<Boolean> = _openQuickRemark
-    val openDownloadDamages: LiveData<Boolean> = _openDownloadDamages
-    val openPrinterSettings: LiveData<Unit> = _openPrinterSettings
-    val openSearch: LiveData<Boolean> = _openSearch
-    val openErrorLogs: LiveData<Unit> = _openErrorLogs
-    val serverUrl:LiveData<String?> = _serverUrl
+    val openConfig: LiveData<Event<Unit>> = _openConfig
+    val openQuickRemark: LiveData<Event<Boolean>> = _openQuickRemark
+    val openDownloadDamages: LiveData<Event<Boolean>> = _openDownloadDamages
+    val openPrinterSettings: LiveData<Event<Unit>> = _openPrinterSettings
+    val openSearch: LiveData<Event<Boolean>> = _openSearch
+    val serverUrl:LiveData<Event<String?>> = _serverUrl
 
 
     fun openConfig() {
-        _openConfig.postValue(Unit)
+        _openConfig.postValue(Event(Unit))
     }
 
     fun openPrinterSetting() {
-        _openPrinterSettings.postValue(Unit)
+        _openPrinterSettings.postValue(Event(Unit))
     }
 
     fun openDownloadDamages() {
         compositeJob = CoroutineScope(appCoroutineDispatchers.db).launch {
             val configured = repository.isConfiguredAsync()
             withContext(appCoroutineDispatchers.main) {
-                _openDownloadDamages.postValue(configured)
+                _openDownloadDamages.postValue(Event(configured))
             }
         }
     }
@@ -57,7 +57,7 @@ class AdminConfigViewModel @Inject constructor(repository: Repository, appCorout
         compositeJob = CoroutineScope(appCoroutineDispatchers.db).launch {
             val configured = repository.isConfiguredAsync()
             withContext(appCoroutineDispatchers.main) {
-                _openQuickRemark.postValue(configured)
+                _openQuickRemark.postValue(Event(configured))
             }
         }
     }
@@ -80,7 +80,7 @@ class AdminConfigViewModel @Inject constructor(repository: Repository, appCorout
         compositeJob = CoroutineScope(appCoroutineDispatchers.db).launch {
             val configured = repository.isConfiguredAsync()
             withContext(appCoroutineDispatchers.main) {
-                _openSearch.postValue(configured)
+                _openSearch.postValue(Event(configured))
             }
         }
     }
@@ -89,7 +89,7 @@ class AdminConfigViewModel @Inject constructor(repository: Repository, appCorout
         compositeJob = CoroutineScope(appCoroutineDispatchers.db).launch {
             val serverUrl = repository.getServerUrl()
             withContext(appCoroutineDispatchers.main){
-                _serverUrl.postValue(serverUrl)
+                _serverUrl.postValue(Event(serverUrl))
             }
         }
     }
@@ -100,6 +100,7 @@ class AdminConfigViewModel @Inject constructor(repository: Repository, appCorout
         }
     }
 
+
     override fun handleDeviceConfigured(configured: Boolean) {
         super.handleDeviceConfigured(configured)
         if (configured) {
@@ -107,9 +108,5 @@ class AdminConfigViewModel @Inject constructor(repository: Repository, appCorout
         } else {
             navigateToLoginIfFirstTime()
         }
-    }
-
-    fun openErrorLogs() {
-        _openErrorLogs.postValue(Unit)
     }
 }
